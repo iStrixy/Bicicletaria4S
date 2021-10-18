cbb22a269382884ab12f134afbe9f2c8b03a073f
SavioTomaz <88664171+SavioTomaz@users.noreply.github.com>
2021-10-18 10:07:18 -0300

    Adicionando "Cadastro_produto.aspx"

---
+++ Cadastro_servico.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PROJ_INTER_BC4S
+{
+    public partial class Cadastro_servico : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCadastrarServico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtValorServico_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtDescricaoServico_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
